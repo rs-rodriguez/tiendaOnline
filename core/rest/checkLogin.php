beab3762d7773567c84e5dc2fd1f83d839182c30
Samuel David Guardado Rodriguez <grd.rodrigez13@gmail.com>
2018-12-02 00:06:22 -0600

    Cambios

--- core/rest/checkLogin.php
+++ core/rest/checkLogin.php
@@ -5,14 +5,19 @@
  * Date: 11/26/2018
  * Time: 11:07 PM
  */
-    session_start(); // se inicia en contexto de session
-    require('../crud/crudUsuario.php');
+require('../crud/crudUsuario.php');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+session_start(); // se inicia en contexto de session
+$input = json_decode(file_get_contents('php://input'));
     $users = new crudUsuario();
     if(isset($_SESSION['email'])){
         $response['msg'] = 'OK';
+        $response['msgses'] = 'OK';
         $response['acceso'] = 'Autorizado';
     }else{
-        $result = $users->validarUsuario($_POST['username']);
+        $result = $users->validarUsuario($input->username);
         if (sizeof($result) > 0){
             $response['msg'] = 'OK';
             $response['acceso'] = 'Autorizado';
